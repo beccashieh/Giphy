f667f00abf65fd324e04730a3779b81bda60815c
BShieh <rebeccasshieh@gmail.com>
2019-04-02 11:54:35 -0400

    wip: corrected links. TODO: research why class is not changing on all when clicked and some gifs not animating.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,18 +2,17 @@ $(document).ready(function () {
     //Array of topics
     var topics = ["cats", "dogs", "birds", "fish", "pandas"];
 
-    //Handles adding topic once user inputs something into textbox.
-    $("#add-topic").on("click", function (event) {
-        event.preventDefault();
-        var choice = $("#choice-input").val().trim();
-        topics.push(choice);
-        renderButtons();
-    })
-
     //Function to display the buttons
     function renderButtons() {
         //Deletes buttons prior to adding new ones, preventing duplicates.
         $(".buttons").empty();
+        //Handles adding topic once user inputs something into textbox.
+        $("#add-topic").on("click", function (event) {
+            event.preventDefault();
+            var choice = $("#choice-input").val().trim();
+            topics.push(choice);
+            renderButtons();
+        })
         //Loops through array of topics
         for (var i = 0; i < topics.length; i++) {
             var selector = $("<button>");
@@ -32,11 +31,12 @@ $(document).ready(function () {
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=I3uUEvU4BlcowhufGXlpP2bKhemQ3mvT&limit=10";
         console.log(queryURL);
 
+
         //Ajax function to pull the gifs from giphy api
         $.ajax({
             url: queryURL,
             method: "GET",
-        }).done(function displayGifs(response) {
+        }).done(function (response) {
             console.log(response);
             var results = response.data;//shows the results of gifs
             console.log("results = " + results);
@@ -50,22 +50,39 @@ $(document).ready(function () {
                 //Adds rating for each gif after the image
                 var gifRating = $("<p>").text("Rating: " + results[i].rating);
                 gifs.append(gifRating);
+                var gifTitle = $("<p>").text("Title: " + results[i].title);
+                gifs.append(gifTitle);
                 //holds the image of each gif.
                 var gifImage = $("<img>");
-                var imageURL = results[i].images_original_url;
+                var imageURL = results[i].images.fixed_height_still.url;
+                var animateURL = results[i].images.fixed_height.url;
                 gifImage.attr("src", imageURL);
                 gifImage.attr("data-still", imageURL);
+                gifImage.attr("data-animate", animateURL);
                 gifImage.attr("data-state", "still");
                 gifImage.addClass("img");
                 gifs.append(gifImage);
                 //each gif is added in front of the previous one.
                 $(".images").prepend(gifs);
             }
-            displayGifs();
+
         })
-
-
+        $(document).on("click", ".img", function () {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
     });
+
 
 
 
